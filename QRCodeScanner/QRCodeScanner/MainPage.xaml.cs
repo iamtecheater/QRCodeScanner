b8b45e2010683ea824cec8a58898eed29f4c37e0
sh1karasu <bhunter@dvt.co.za>
2021-03-25 00:55:55 +0200

    first commit

---
+++ QRCodeScanner/QRCodeScanner/MainPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using ZXing;
+
+namespace QRCodeScanner
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        public void scanView_OnScanResult(Result result)
+        {
+            Device.BeginInvokeOnMainThread(async () =>
+            {
+                await DisplayAlert("Scanned result", "The barcode's text is " + result.Text + ". The barcode's format is " + result.BarcodeFormat, "OK");
+            });
+        }
+    }
+}
